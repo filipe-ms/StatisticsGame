28eaa1ad8b98f833aedfdb4fd3138c7435866851
Filipe <lfms@cesar.school>
2025-11-24 19:59:10 -0300

    Background fix

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import BlackjackGame from "./game/game";
 
 export default function Home() {
 	return (
-		<div className="w-full h-full">
-			<main className="w-full h-full">
+		<div className="w-full h-full bg-[#095A2A]">
+			<main className="w-full h-full bg-[#095A2A]">
 				<BlackjackGame />
 			</main>
 		</div>
